4ac83c2749d82b81282597f591f45df43d9c4d8e
carolinealberti <carol_13alberti@hotmail.com>
2023-11-19 11:38:51 -0300

    Correção tarefa calculadora imc

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,17 @@ form.addEventListener('submit', function(event) {
 
     document.getElementById('infos').classList.remove('hidden');
 
-    if (bmi<18.5) {
-        description = "Cuidado! Você está abaixo do peso!";
+    if (bmi < 18.5) {
+        description = 'Cuidado! Você está abaixo do peso!';
     } else if (bmi >= 18.5 && bmi <= 25) {
         description = "Você está no peso ideal!";
         value.classList.remove('attention');
         value.classList.add('normal');
-    } else if (bmi > 25 && bmi <= 30) {
-        description = "Cuidado! Você está com sobre peso!";
-    } else if (bmi > 30 && bmi <= 35) {
+    } else if (bmi >= 25 && bmi <= 30) {
+        description = "Cuidado! Você está com sobrepeso!";
+    } else if (bmi >= 30 && bmi <= 35) {
         description = "Cuidado! Você está com obesidade moderada!";
-    } else if (bmi > 35 && bmi <= 40) {
+    } else if (bmi >= 35 && bmi <= 40) {
         description = "Cuidado! Você está com obesidade severa!";
     } else {
         description = "Cuidado! Você está com obesidade morbida!";
